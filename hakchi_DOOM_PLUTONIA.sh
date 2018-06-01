43ac979ecfd23b1e9ae10d30518fd7f566666663
Ryan 'Swingflip' Hamlin <ryan@ryanhamlin.co.uk>
2018-06-01 13:40:41 +0100

    Added Plutonia files.

---
+++ hakchi_DOOM_PLUTONIA.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+source /etc/preinit
+script_init
+
+WorkingDir=$(pwd)
+GameName=$(echo $WorkingDir | awk -F/ '{print $NF}')
+ok=0
+
+if [ -f "/usr/share/games/$GameName/$GameName.desktop" ]; then
+	DOOMTrueDir=$(grep /usr/share/games/$GameName/$GameName.desktop -e 'Exec=' | awk '{print $2}' | sed 's/\([/\t]\+[^/\t]*\)\{1\}$//')
+	DOOMPortableCore="$DOOMTrueDir/etc/libretro/core/prboom"
+	DOOMPortableFiles="$DOOMTrueDir/DOOM_PLUTONIA_files"
+	ok=1
+fi
+
+if [ ! -f "$DOOMPortableFiles/PLUTONIA.WAD" ]; then
+	ok=0
+fi
+
+if [ "$ok" == 1 ]; then
+	decodepng "$DOOMTrueDir/Hakchi_DOOM_PLUTONIA_assets/doom_plutonia_splash-min.png" > /dev/fb0;
+	[ -f "$rootfs/share/retroarch/assets/RAloading-min.png" ] && mount_bind "$DOOMTrueDir/Hakchi_DOOM_PLUTONIA_assets/doom_plutonia_splash-min.png" "$rootfs/share/retroarch/assets/RAloading-min.png"
+	exec retroarch-clover "../../..$DOOMPortableCore" "$DOOMPortableFiles/PLUTONIA.WAD"
+	umount "$rootfs/share/retroarch/assets/RAloading-min.png"
+else	
+	decodepng "$DOOMTrueDir/Hakchi_DOOM_PLUTONIA_assets/doomerror_files-min.png" > /dev/fb0;
+	sleep 5
+fi

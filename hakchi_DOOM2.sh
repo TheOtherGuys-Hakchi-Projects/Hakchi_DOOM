b78466909b31e929e1b4b8f3c74f6bb1dabf25cc
bslenul <33353403+bslenul@users.noreply.github.com>
2018-06-13 14:43:08 +0200

    Changes.
    -Added prboom.wad.
    -No mount for splashscreen.
    -Ignore case for "DOOM2.WAD".

--- hakchi_DOOM2.sh
+++ hakchi_DOOM2.sh
@@ -11,18 +11,13 @@ if [ -f "/usr/share/games/$GameName/$GameName.desktop" ]; then
 	DOOMTrueDir=$(grep /usr/share/games/$GameName/$GameName.desktop -e 'Exec=' | awk '{print $2}' | sed 's/\([/\t]\+[^/\t]*\)\{1\}$//')
 	DOOMPortableCore="$DOOMTrueDir/etc/libretro/core/prboom"
 	DOOMPortableFiles="$DOOMTrueDir/DOOM_2_files"
-	ok=1
-fi
-
-if [ ! -f "$DOOMPortableFiles/doom2.wad" ]; then
-	ok=0
+	wad=$(ls $DOOMPortableFiles | grep -i doom2.wad)
+	[ ! -z "$wad" ] && ok=1
 fi
 
 if [ "$ok" == 1 ]; then
 	decodepng "$DOOMTrueDir/Hakchi_DOOM2_assets/doom2splash-min.png" > /dev/fb0;
-	[ -f "$rootfs/share/retroarch/assets/RAloading-min.png" ] && mount_bind "$DOOMTrueDir/Hakchi_DOOM2_assets/doom2splash-min.png" "$rootfs/share/retroarch/assets/RAloading-min.png"
-	exec retroarch-clover "../../..$DOOMPortableCore" "$DOOMPortableFiles/doom2.wad"
-	umount "$rootfs/share/retroarch/assets/RAloading-min.png"
+	exec retroarch-clover "../../..$DOOMPortableCore" "$DOOMPortableFiles/$wad" --custom-loadscreen "../../../../../../..$DOOMTrueDir/Hakchi_DOOM2_assets/doom2splash-min.png"
 else
 	decodepng "$DOOMTrueDir/Hakchi_DOOM2_assets/doomerror_files-min.png" > /dev/fb0;
 	sleep 5

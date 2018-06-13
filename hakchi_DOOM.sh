ed4556d13da040032339ccc5eab397834d36ddc0
bslenul <33353403+bslenul@users.noreply.github.com>
2018-06-13 13:50:00 +0200

    Revert while keeping some stuff.
    -Revert to Swing's way.
    -Keeping no mount change.
    -Keeping the wad filename changes.

--- hakchi_DOOM.sh
+++ hakchi_DOOM.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-ok=0
-DOOMPortableCore="$(dirname $0)/etc/libretro/core/prboom"
-DOOMPortableFiles="$(dirname $0)/DOOM_1_files"
-DOOMPortableAssets="$(dirname $0)/Hakchi_DOOM_assets"
-wad=$(ls $DOOMPortableFiles | grep -i doom.wad)
-[ -z "$wad" ] && wad=$(ls $DOOMPortableFiles | grep -i doom1.wad)
+source /etc/preinit
+script_init
 
-[ ! -z "$wad" ] && ok=1
+WorkingDir=$(pwd)
+GameName=$(echo $WorkingDir | awk -F/ '{print $NF}')
+ok=0
+
+if [ -f "/usr/share/games/$GameName/$GameName.desktop" ]; then
+	DOOMTrueDir=$(grep /usr/share/games/$GameName/$GameName.desktop -e 'Exec=' | awk '{print $2}' | sed 's/\([/\t]\+[^/\t]*\)\{1\}$//')
+	DOOMPortableCore="$DOOMTrueDir/etc/libretro/core/prboom"
+	DOOMPortableFiles="$DOOMTrueDir/DOOM_1_files"
+	wad=$(ls $DOOMPortableFiles | grep -i doom.wad)
+	[ -z "$wad" ] && wad=$(ls $DOOMPortableFiles | grep -i doom1.wad)
+	[ ! -z "$wad" ] && ok=1
+fi
 
 if [ "$ok" == 1 ]; then
-	decodepng "$DOOMPortableAssets/doom1splash-min.png" > /dev/fb0;
-	exec retroarch-clover "../../..$DOOMPortableCore" "$DOOMPortableFiles/$wad" --custom-loadscreen "../../../../../../../$DOOMPortableAssets/doom1splash-min.png"
+	decodepng "$DOOMTrueDir/Hakchi_DOOM_assets/doom1splash-min.png" > /dev/fb0;
+	exec retroarch-clover "../../..$DOOMPortableCore" "$DOOMPortableFiles/$wad" --custom-loadscreen "../../../../../../..$DOOMTrueDir/Hakchi_DOOM_assets/doom1splash-min.png"
 else
-	decodepng "$DOOMPortableAssets/doomerror_files-min.png" > /dev/fb0;
+	decodepng "$DOOMTrueDir/Hakchi_DOOM_assets/doomerror_files-min.png" > /dev/fb0;
 	sleep 5
 fi

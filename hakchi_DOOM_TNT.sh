f61eeda78920afcf316f325b920ed029aa40ac19
bslenul <33353403+bslenul@users.noreply.github.com>
2018-06-13 14:46:51 +0200

    Ignore case for "TNT.WAD".

--- hakchi_DOOM_TNT.sh
+++ hakchi_DOOM_TNT.sh
@@ -11,16 +11,13 @@ if [ -f "/usr/share/games/$GameName/$GameName.desktop" ]; then
 	DOOMTrueDir=$(grep /usr/share/games/$GameName/$GameName.desktop -e 'Exec=' | awk '{print $2}' | sed 's/\([/\t]\+[^/\t]*\)\{1\}$//')
 	DOOMPortableCore="$DOOMTrueDir/etc/libretro/core/prboom"
 	DOOMPortableFiles="$DOOMTrueDir/DOOM_TNT_files"
-	ok=1
-fi
-
-if [ ! -f "$DOOMPortableFiles/TNT.WAD" ]; then
-	ok=0
+	wad=$(ls $DOOMPortableFiles | grep -i tnt.wad)
+	[ ! -z "$wad" ] && ok=1
 fi
 
 if [ "$ok" == 1 ]; then
 	decodepng "$DOOMTrueDir/Hakchi_DOOM_TNT_assets/doom_tnt_splash-min.png" > /dev/fb0;
-	exec retroarch-clover "../../..$DOOMPortableCore" "$DOOMPortableFiles/TNT.WAD" --custom-loadscreen ../../../../../../../../$DOOMTrueDir/Hakchi_DOOM_TNT_assets/doom_tnt_splash-min.png
+	exec retroarch-clover "../../..$DOOMPortableCore" "$DOOMPortableFiles/$wad" --custom-loadscreen "../../../../../../..$DOOMTrueDir/Hakchi_DOOM_TNT_assets/doom_tnt_splash-min.png"
 else	
 	decodepng "$DOOMTrueDir/Hakchi_DOOM_TNT_assets/doomerror_files-min.png" > /dev/fb0;
 	sleep 5
